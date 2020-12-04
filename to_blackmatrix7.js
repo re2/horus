f9bb0682027fe05dd62d6305a998ca57759785fa
codetracer <75488008+codetracer@users.noreply.github.com>
2020-12-04 23:42:33 +0800

    修改替换文件的配置

--- to_blackmatrix7.js
+++ to_blackmatrix7.js
@@ -8,11 +8,11 @@ function replaceStr(file){
     let filename = path.basename(file);
     if (extname !== '.jpg' && extname !== '.png' && extname !== '.gif' && filename !== 'to_blackmatrix7.js' && filename !== 'to_codetracer.js'){
         fs.readFile(file, {encoding:'utf-8'}, function(err, data){
-            data = data.replace(/https?:\/\/raw\.githubusercontent\.com\/codetracer\/prophet\/master/g, 'https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master');
-            data = data.replace(/https?:\/\/github\.com\/codetracer\/prophet\/tree\/master\//g, 'https://github.com/blackmatrix7/ios_rule_script/tree/master/');
-            data = data.replace(/https?:\/\/github\.com\/codetracer\/prophet\/blob\/master\//g, 'https://github.com/blackmatrix7/ios_rule_script/blob/master/');
+            data = data.replace(/https?:\/\/raw\.githubusercontent\.com\/codetracer\/codhorus\/master/g, 'https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master');
+            data = data.replace(/https?:\/\/github\.com\/codetracer\/codhorus\/tree\/master\//g, 'https://github.com/blackmatrix7/ios_rule_script/tree/master/');
+            data = data.replace(/https?:\/\/github\.com\/codetracer\/codhorus\/blob\/master\//g, 'https://github.com/blackmatrix7/ios_rule_script/blob/master/');
             data = data.replace(/codetracer/g, 'blackmatrix7');
-            data = data.replace(/prophet/g, 'ios_rule_script');
+            data = data.replace(/horus/g, 'ios_rule_script');
             data = data.replace(/idreamtalk/g, 'blackmatrix7');
             data = data.replace(/dreamboat/g, 'ios_rule_script');
             fs.writeFile(file, data, function(err){
