14216ca7d80f8a128d72837bba51c0bc435a0190
codetracer <75488008+codetracer@users.noreply.github.com>
2020-12-05 17:52:59 +0800

    调试我的页面处理脚本执行情况

--- script/bilibili/bilibili_plus.js
+++ script/bilibili/bilibili_plus.js
@@ -83,7 +83,7 @@ let magicJS = MagicJS(scriptName, 'INFO');
       // 我的页面处理，去除一些推广按钮
       case /^https?:\/\/app\.bilibili\.com\/x\/v2\/account\/mine/.test(magicJS.request.url):
         try{
-          magicJS.notify('触发我的页面处理');
+          magicJS.notify('我的页面处理触发');
           const item0List = ['离线缓存', '历史记录', '我的收藏', '稍后再看'];
           const item1List = ['创作首页', '创作学院', '打卡挑战'];
           const item2List = ['我的课程', '个性装扮', '我的钱包', '直播中心'];
@@ -101,6 +101,7 @@ let magicJS = MagicJS(scriptName, 'INFO');
           let items3 = obj['data']['sections_v2'][3]['items'].filter((e) =>{return item3List.indexOf(e.title)>=0;});
           obj['data']['sections_v2'][3]['items'] = items3;
           body = JSON.stringify(obj);
+          magicJS.notify('我的页面处理完成');
         }
         catch (err){
           magicJS.logError(`我的页面处理出现异常：${err}`);
